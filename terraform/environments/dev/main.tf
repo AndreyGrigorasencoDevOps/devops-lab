0399c76f0fc451b46548b599949f315e8fffad04
Andrei Grigorasenco <andrey.grigorasenco@gmail.com>
2026-02-28 02:01:39 +0000

    chore(ci): remove infra steps from azure deployment workflow

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,89 @@
+locals {
+  tags = var.tags
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "taskapi-${var.env}-rg-uks"
+  location = var.location
+  tags     = local.tags
+}
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "taskapi-${var.env}-law-uks"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = local.tags
+}
+
+resource "azurerm_container_app_environment" "main" {
+  name                       = "taskapi-${var.env}-cae-uks"
+  location                   = azurerm_resource_group.main.location
+  resource_group_name        = azurerm_resource_group.main.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
+  tags                       = local.tags
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+resource "azurerm_container_registry" "main" {
+  name                = "taskapi${var.env}acr${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = false
+  tags                = local.tags
+}
+
+resource "azurerm_container_app" "main" {
+  name                         = "taskapi-${var.env}-app"
+  container_app_environment_id = azurerm_container_app_environment.main.id
+  resource_group_name          = azurerm_resource_group.main.name
+  revision_mode                = "Single"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  registry {
+    server   = azurerm_container_registry.main.login_server
+    identity = "SystemAssigned"
+  }
+
+  template {
+    container {
+      name   = "task-api"
+      image  = "${azurerm_container_registry.main.login_server}/task-api:${var.env}"
+      cpu    = 0.5
+      memory = "1Gi"
+
+      env {
+        name  = "NODE_ENV"
+        value = "production"
+      }
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 3000
+
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.main.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_container_app.main.identity[0].principal_id
+}
